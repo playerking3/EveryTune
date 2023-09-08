383905ab45daa97ee0a6733367ffd47569c017b3
playerking3 <murilomarquesdecastro@gmail.com>
2023-09-08 11:26:38 -0300

    estilização e funcionalidade
    Comecei a estilização da main e adicionei a funcionalidade com a API do deezer

--- assets/script/indexScript.js
+++ assets/script/indexScript.js
@@ -1,1 +1,21 @@
+pesquisa = document.getElementById("searchForm")
 
+pesquisa.addEventListener("submit",  function(event) {
+	event.preventDefault()
+	let data = new FormData(pesquisa)
+	let nome = data.get("search")
+	APIRequest(nome)
+})
+
+function APIRequest(musica){
+	let url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q="+musica
+
+	 fetch(url)
+        .then(function(response){
+            return response.json()
+        })
+
+        .then(function(data){
+            console.log(data)
+        })
+}
