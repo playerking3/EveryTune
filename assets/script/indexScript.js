befcf6642f6f4c0f860f7d03ca2c7ecfcf55caaa
playerking3 <murilomarquesdecastro@gmail.com>
2023-10-20 15:45:27 -0300

    aasas

--- assets/script/indexScript.js
+++ assets/script/indexScript.js
@@ -15,7 +15,18 @@ function APIRequest(musica){
 	capa.style.animationName = 'slideOut'
 	$("#disco").animate({"left": "50%"})
 	document.getElementById("box").style.transform = "translateX(0%)"
-	fetch(url, { mode: 'cors' })
+
+	var myHeaders = new Headers();
+	myHeaders.append("Cookie", "_abck=1A804339439688BFA7F945E0ECB43B1C~-1~YAAQlmvcF2kgCD6LAQAA3s9lTgruCuYM3a7ft1GaS4ypsdwqbfkVb5FNB128ohJGkVakGYCnk05/UHr0e6B24LclRiKiidomtYRQmzNsaAjeMiDTgGQpOuYfgrMHsTXQ04cjdTe6bdv2+Lklp4tzI8MNO7XTuXQxTs07/tmdHTHwDCz2+UVchucu33v1607c34asszySSNpd0dQMtA8t6eH0sBwR4l7UssnmWupN+Irm/tzbpGV/MCt/1osI7UCDXNPLX/jxxpr02tLqSdKTJf4GnyfrQb+j1OfYsn+DJ/fxicckIoLI1qEesEuxXEh0U9sueAPkYaLlCYZjOscBUD5JrLnVPiGvzyoPhd1y0zV4IeDkIUvnAc4=~-1~-1~-1; bm_sz=7863E7CB0EA33FA59291FBFC06F1622F~YAAQlmvcF2ogCD6LAQAA3s9lThUpmclQa41VsoD1zLGaD3oLW4o7Q4yWdINTyIkfTSG0PAG78vCUBwi7yGIOg81gKgkZIW9l8z35RXYCiNFoHSvGbtXNIF5UPfqDjy5ygyq3c1GoZC8deXYzxEwRp2FtBVLqiokm2gfkS7z3dg9r0YHh2g/XJCzUDM4IY14iWkX//ehzwC+Xv+75dq0ICc8D/Fly4Cr+d8cnlZHeOPmoKlZZa07QA80cdXqG7LRYTeyQwiZwiqdrGQhl0uQlfuXsw4KINnFyaFc0r3VLPtckr2g=~4404791~4273465; dzr_uniq_id=dzr_uniq_id_frff6abf7863783f0d8d5b0add0464f7c42b5303");
+
+	var requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+	};
+
+
+	fetch(url, requestOptions)
 
         .then(function(response){
         	console.log('esperando')
